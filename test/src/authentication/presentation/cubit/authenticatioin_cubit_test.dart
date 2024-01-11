d48ca3a596c3b93fc48772608228dafad0775613
Sergey Kazberovich <kazbers@gmail.com>
2024-01-11 13:15:01 +0400

    test authenticationCubit createUser method

--- test/src/authentication/presentation/cubit/authenticatioin_cubit_test.dart
+++ test/src/authentication/presentation/cubit/authenticatioin_cubit_test.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:tdd_tutorial/src/authentication/domain/usecases/create_user.dart';
@@ -13,10 +15,13 @@ void main() {
   late CreateUser createUser;
   late AuthenticationCubit cubit;
 
+  const tCreateUserParams = CreateUserParams.empty();
+
   setUp(() {
     getUsers = MockGetUsers();
     createUser = MockCreateUser();
     cubit = AuthenticationCubit(createUser: createUser, getUsers: getUsers);
+    registerFallbackValue(tCreateUserParams);
   });
 
   tearDown(() => cubit.close());
@@ -24,5 +29,31 @@ void main() {
   test('initial state should be [AuthenticationInitial]', () async {
     // Assert
     expect(cubit.state, const AuthenticationInitial());
+  });
+
+  group('createUser', ()  {
+    // even if it's cubit we use bloc_test
+    blocTest<AuthenticationCubit, AuthenticationState>(
+        'should emit [CreatingUser, UserCreated] when successful',
+        build: () {
+          when(() => createUser(any())).thenAnswer(
+            (_) async => const Right(null),
+          );
+
+          return cubit;
+        },
+        act: (cubit) => cubit.createUser(
+              createdAt: tCreateUserParams.createdAt,
+              name: tCreateUserParams.name,
+              avatar: tCreateUserParams.avatar,
+            ),
+        expect: () => const [
+              CreatingUser(),
+              UserCreated(),
+            ],
+        verify: (_) {
+          verify(() => createUser(tCreateUserParams)).called(1);
+          verifyNoMoreInteractions(createUser);
+        });
   });
 }
